d77ba79377655a592486b31446cb567203b0931c
vagineer gaming <randompackards@gmail.com>
2023-10-05 18:46:02 +0400

    s

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//  variables
+
+//string
+let userPrimaryName = 'damon';
+let userSecondaryName = 'hill';
+
+//number
+let userAge = 63;
+
+
+let userInfo = "i" + " " + "am" + " " + userPrimaryName + " " + userSecondaryName;
+
+let userAgeInfo = "i" + " " + "am" + " " + userAge + " " + "years" + " " + "old";
+
+console.log(userInfo);
+
+console.log(userAgeInfo);
